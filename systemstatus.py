366080a119d7df786989b990e18d725956375e07
devops <yangzhiguang>
2017-12-12 17:38:26 +0800

    fix email sender

--- systemstatus.py
+++ systemstatus.py
@@ -41,49 +41,53 @@ def isset(list_arr,name):
 print_str="";
 
 #获取系统内存使用情况
-if (print_type==1) or isset(sys.argv,"mem"):
-    memory_convent=1024*1024
-    mem=psutil.virtual_memory()
-    print_str+="内存状态如下:\n"
-    print_str=print_str+"系统的内存容量为: "+str( mem.total/( memory_convent ) ) + " MB\n"
-    print_str = print_str + "   系统的内存以使用容量为: " + str(mem.used / (memory_convent)) + " MB\n"
-    print_str = print_str + "   系统可用的内存容量为: " + str(mem.total / (memory_convent) - mem.used / (1024 * 1024)) + "MB\n"
-    print_str = print_str + "   内存的buffer容量为: " + str(mem.buffers / (memory_convent)) + " MB\n"
-    print_str = print_str + "   内存的cache容量为:" + str(mem.cached / (memory_convent)) + " MB\n"
+def meminfo():
+    if (print_type==1) or isset(sys.argv,"mem"):
+        memory_convent=1024*1024
+        mem=psutil.virtual_memory()
+        print_str+="内存状态如下:\n"
+        print_str=print_str+"系统的内存容量为: "+str( mem.total/( memory_convent ) ) + " MB\n"
+        print_str = print_str + "   系统的内存以使用容量为: " + str(mem.used / (memory_convent)) + " MB\n"
+        print_str = print_str + "   系统可用的内存容量为: " + str(mem.total / (memory_convent) - mem.used / (1024 * 1024)) + "MB\n"
+        print_str = print_str + "   内存的buffer容量为: " + str(mem.buffers / (memory_convent)) + " MB\n"
+        print_str = print_str + "   内存的cache容量为:" + str(mem.cached / (memory_convent)) + " MB\n"
 
 #获取cpu的相关信息
-if (print_type==1) or isset(sys.argv,"cpu"):
-    print_str+="cpu状态如下:\n"
-    cpu_status=psutil.cpu_times()
-    print_str = print_str + "   user = " + str(cpu_status.user) + "\n"
-    print_str = print_str + "   nice = " + str(cpu_status.nice) + "\n"
-    print_str = print_str + "   system = " + str(cpu_status.system) + "\n"
-    print_str = print_str + "   idle = " + str(cpu_status.idle) + "\n"
-    print_str = print_str + "   iowait = " + str(cpu_status.iowait) + "\n"
-    print_str = print_str + "   irq = " + str(cpu_status.irq) + "\n"
-    print_str = print_str + "   softirq = " + str(cpu_status.softirq) + "\n"
-    print_str = print_str + "   steal = " + str(cpu_status.steal) + "\n"
-    print_str = print_str + "   guest = " + str(cpu_status.guest) + "\n"
+def cpuinfo():
+    if (print_type==1) or isset(sys.argv,"cpu"):
+        print_str+="cpu状态如下:\n"
+        cpu_status=psutil.cpu_times()
+        print_str = print_str + "   user = " + str(cpu_status.user) + "\n"
+        print_str = print_str + "   nice = " + str(cpu_status.nice) + "\n"
+        print_str = print_str + "   system = " + str(cpu_status.system) + "\n"
+        print_str = print_str + "   idle = " + str(cpu_status.idle) + "\n"
+        print_str = print_str + "   iowait = " + str(cpu_status.iowait) + "\n"
+        print_str = print_str + "   irq = " + str(cpu_status.irq) + "\n"
+        print_str = print_str + "   softirq = " + str(cpu_status.softirq) + "\n"
+        print_str = print_str + "   steal = " + str(cpu_status.steal) + "\n"
+        print_str = print_str + "   guest = " + str(cpu_status.guest) + "\n"
 
 
 # 查看硬盘基本信息
-if (print_type == 1) or isset(sys.argv, "disk"):
-    print_str += " 硬盘信息如下:\n"
-    disk_status = psutil.disk_partitions()
-    for item in disk_status:
-        print_str = print_str + "   " + str(item) + "\n"
+def diskinfo():
+    if (print_type == 1) or isset(sys.argv, "disk"):
+        print_str += " 硬盘信息如下:\n"
+        disk_status = psutil.disk_partitions()
+        for item in disk_status:
+            print_str = print_str + "   " + str(item) + "\n"
 
 # 查看当前登录的用户信息
-if (print_type == 1) or isset(sys.argv, "user"):
-    print_str += " 登录用户信息如下:\n "
-    user_status = psutil.users()
-    for item in user_status:
-        print_str = print_str + "   " + str(item) + "\n"
+def userinfo():
+    if (print_type == 1) or isset(sys.argv, "user"):
+        print_str += " 登录用户信息如下:\n "
+        user_status = psutil.users()
+        for item in user_status:
+            print_str = print_str + "   " + str(item) + "\n"
 
-print_str += "---------------------------------------------------------------\n"
-print(print_str)
-handle.write(print_str)
-handle.close()
+    print_str += "---------------------------------------------------------------\n"
+    print(print_str)
+    handle.write(print_str)
+    handle.close()
 
 
 
@@ -112,5 +116,5 @@ def send_mail2(SMTP_host,from_account,from_passwd,to_account,subject,content):
     msg['To']=to_account
     email_client.sendmail(from_account,to_account,msg.as_string())
     email_client.quit()
-if __name__== '__main__':
-    sendEmail()
+#if __name__== '__main__':
+#   sendEmail()
