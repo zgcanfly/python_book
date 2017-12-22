d9f1cc0d2fc5854e5aa7fb015e5a28503cdd08a5
devops <yangzhiguang>
2017-12-22 19:05:57 +0800

    add script files

--- jenkins_function.py
+++ jenkins_function.py
@@ -2,5 +2,16 @@
 #jenkins中匿名用户需要有项目可读权限
 import jenkins
 jenkins_server_url='http://jenkins.stockalert.cn'
-server=jenkins.Jenkins(url=jenkins_server_url,username='zgyang',password='f4f892e1871ae8b437ca4c990895b5ca')
-print(server.get_version())
+jenkins_id='zgyang'
+jenkins_token='f4f892e1871ae8b437ca4c990895b5ca'
+server=jenkins.Jenkins(url=jenkins_server_url,username=jenkins_id,password=jenkins_token)
+# job_name='dev_nodejs'
+# server.build_job(job_name)
+# server.build_job(job_name,parameters=param_dict)
+#
+# server.get_job_info(job_name)['lastBuild']['number']
+# jobs=server.get_jobs()
+# print(jobs)
+# my_job=server.get_job_config('Ansible SSH List Update')
+# print(my_job)
+server.build_job('Ansible SSH List Update')
