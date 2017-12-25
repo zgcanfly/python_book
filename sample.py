0d12f755bffe9d0255732fac90e954292a9dfb8e
devops <yangzhiguang>
2017-12-25 19:25:12 +0800

    add script files

--- sample.py
+++ sample.py
@@ -10,6 +10,7 @@ jenkins_token='f4f892e1871ae8b437ca4c990895b5ca'
 server=jenkins.Jenkins(url=jenkins_server_url,username=jenkins_id,password=jenkins_token)
 project=''
 
+
 def deploy(project):
     server.build_job(project)
     info = server.get_job_info(project)['lastBuild']
@@ -19,6 +20,8 @@ def deploy(project):
     tinfo = server.get_build_info(project, number)
     return tinfo
 
+def RemoveChinese(strValue):
+    pass
 
 
 def sendEmail(content):  # 定义邮件报警
@@ -41,6 +44,14 @@ def sendEmail(content):  # 定义邮件报警
         print(e)
 
 def onQQMessage(bot, contact, member, content):
+    #获取项目列表及发布列表
+    project_list = []
+    deploy_list = []
+    joblist = server.get_all_jobs()
+    for i in range(len(joblist)):
+        project_list.append(joblist[i]['name'])
+        deploy_list.append('发布'+joblist[i]['name'])
+
 #文字聊天部分
     if content == '天气怎么样':
         bot.SendTo(contact, '今天天气不错，晚上可以看星星呢')
@@ -50,28 +61,22 @@ def onQQMessage(bot, contact, member, content):
         sendEmail(content)
         bot.SendTo(contact, '什么邮件')
  #jenkins部分
+    elif content == '项目列表':
+        bot.SendTo(contact, str(project_list))
     elif content == '发布列表':
-        project_list=[]
-        deploylist = server.get_all_jobs()
-        for i in range(len(deploylist)):
-            project_list.append(deploylist[i]['name'])
-            bot.SendTo(contact,project_list)
-
-    elif content == '发布' + project:
-        #项目列表
-        project_list=[]
-        deploylist = server.get_all_jobs()
-        for i in range(len(deploylist)):
-            project_list.append(deploylist[i]['name'])
-        #是否在项目列表中
-        if project not in project_list:
-            bot.SendTo(contact,'没有'+project+'这个项目')
-
+        bot.SendTo(contact, str(deploy_list))
+    elif content == '测试发布':
+        bot.SendTo(contact,str(project_list))
+        bot.SendTo(contact,str(deploy_list))
+#发布项目
+    if content in deploy_list:
+        project=content[2:]
         deploystatus = deploy(project)
         print(deploystatus['result'])
         if deploystatus['result'] == 'SUCCESS':
             bot.SendTo(contact, '项目发布成功')
         else:
             bot.SendTo(contact, '项目发布失败' + deploystatus['url'])
+
 
 #生活小助手部分
