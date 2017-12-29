e6cbd94a33960d7e6ba7e0cf23f13e0aebbac5ac
devops <yangzhiguang>
2017-12-29 14:06:56 +0800

    up fiels

--- createtensorflow.py
+++ createtensorflow.py
@@ -1,7 +1,12 @@
 #coding=utf-8
 import tensorflow as tf
-m1 = tf.constant([3,3])
+m1 = tf.constant([[3,3]])
 m2 = tf.constant([[2],[3]])
 #创建一个矩阵乘法
 product = tf.matmul(m1,m2)
-print(product)
+
+#定义会话
+#定义图
+with tf.Session() as sess:
+    result=sess.run(product)
+    print(result)
