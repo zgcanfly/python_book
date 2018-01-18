ed396fc347985f994d2c0c4f9c08f5a24d4f4690
codebayby <2467815216@qq.com>
2018-01-18 17:05:54 +0800

    up files

--- download.py
+++ download.py
@@ -1,8 +1,15 @@
-#coding=utf-8
 import urllib.request
 import sys
 import os
+import requests
+import io
+import re
+from  bs4 import BeautifulSoup
 import threading
+
+url = 'https://jp.pornhub.com/view_video.php?viewkey=ph597ac889773f6'
+
+
 def callbackfunc(blocknum, blocksize, totalsize):
     '''回调函数
     @blocknum: 已经下载的数据块
@@ -15,7 +22,7 @@ def callbackfunc(blocknum, blocksize, totalsize):
         percent = 100
     downsize=blocknum * blocksize
     if downsize >= totalsize:
-        downsize=totalsize
+    	downsize=totalsize
     s ="%.2f%%"%(percent)+"====>"+"%.2f"%(downsize/1024/1024)+"M/"+"%.2f"%(totalsize/1024/1024)+"M \r"
     sys.stdout.write(s)
     sys.stdout.flush()
@@ -26,5 +33,22 @@ def downimg():
     url='http://dlsw.baidu.com/sw-search-sp/soft/e7/10520/KanKan_V2.7.8.2126_setup.1416995191.exe'
     filename=os.path.basename(url)
     urllib.request.urlretrieve(url, filename, callbackfunc)
-#启动线程下载
-threading.Thread(target=downimg,args=('')).start()
+
+
+def getporhub():
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+    try:
+        r = requests.get(url, timeout=30)
+    except requests.RequestException as e:
+        print("网页失败请求！")
+    r.raise_for_status()
+    r.encoding = 'utf-8'
+
+    print(r)
+
+
+
+if __name__=='__main__':
+    getporhub()
+    # 启动线程下载
+    # threading.Thread(target=downimg,args=('')).start()
