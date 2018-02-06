d3477314dd1adca42675e27ead0c2b6efba68495
codebayby <2467815216@qq.com>
2018-02-07 00:16:45 +0800

    更新代码

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 import sys
 import  pygame
-from settings import Settings
-
+from settings import  Settings
 
 def run_game():
 	pygame.init()
-	screen = pygame.display.set_mode((1200,800))
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 	bg_color=(230,230,230)
 
@@ -13,6 +14,6 @@ def run_game():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				sys.exit()
-		screen.fill(bg_color)
+		screen.fill(ai_settings.bg_color)
 		pygame.display.flip()
 run_game()
