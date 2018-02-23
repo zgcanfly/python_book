e3e581658bf999fc7951630739eb5ac0c194ad85
codebayby <2467815216@qq.com>
2018-02-23 23:19:56 +0800

    删除无用代码

--- test.py
+++
@@ -1,5 +0,0 @@
-import tushare as ts
-# a = ts.get_k_data('600000')
-tickers = ts.get_k_data(6000)
-print(tickers)
-
---
+++ web/web.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def sayhello():
+	return 'hello'
+@app.route('/hello')
+def hello():
+    users = [{"username":"users1","url":"/users/user1"},
+             {"username":"users2","url":"/users/user2"}]
+    return render_template("hello.html",title="User List",users=users)
+@app.route('/index')
+def index():
+	return render_template("index.html")
+if __name__ == '__main__':
+    app.run(debug=True)
